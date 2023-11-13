617d4bb0c86c8b139c892503003a2e1d91b50f68
McShriBabushka <reeshavthealpha@gmail.com>
2023-11-14 01:15:12 +0530

    profile page UI

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,7 +1,9 @@
 import React from 'react'
 import {FaLinkedin, FaSearch} from 'react-icons/fa'
 import { Link } from 'react-router-dom'
+import {useSelector} from 'react-redux'
 export default function Header() {
+  const {currentUser}= useSelector(state=>state.user);
   return (
     <header className='bg-slate-200 shadow-md'>
         <div className='flex justify-between items-center max-w-6xl mx-auto p-3'>
@@ -16,15 +18,18 @@ export default function Header() {
              className='bg-transparent focus:outline-none text-slate-400 w-24 sm:w-64'/>
              <FaSearch className='text-slate-600'></FaSearch>
         </form>
-        <ul className='flex gap-4'>
+        <ul className='flex gap-4 '>
           <Link to='/'>
-           <li className='hidden sm:inline text-slate-700 hover:underline'>Home</li>
+           <li className=' hidden sm:inline text-slate-700 hover:underline'>Home</li>
            </Link>
            <Link to='/about'>
-           <li className='text-slate-700 hover:underline'>About</li>
+           <li className='    text-slate-700 hover:underline'>About</li>
            </Link>
            <Link to='/sign-in'>
-           <li className='text-slate-700 hover:underline'>Sign In</li>
+            {currentUser? (
+              <img className='rounded-full h-9 w-9 object-cover 'src={currentUser.avatar} alt='profile'/>
+            ) : (
+           <li className='text-slate-700 hover:underline'>Sign In</li>)}
            </Link>
         </ul>
         </div>
